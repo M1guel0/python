74cac7835ca0c19019a53a746095d7df920adefb
M1guel0 <miguelangel-0419@hotmail.com>
2025-10-15 15:22:26 -0500

    tipado de dinamico

--- tutorialMidudev.py
+++ tutorialMidudev.py
@@ -7,11 +7,19 @@ print ("Que edad tienes??")
 edad = input()
 print (edad)
 
-nombre = input ("Hola como te llamas??\n")
+nombre = input ("Hola como te llamas??\n") #Salto de linea cuando. sin el \n se escribe en la misma linea de comando 
 print (nombre)
 
 edad = input ("Qué edad tienes ??\n")
 print (edad)
+
+##tIPADO DINAMICO, ES DECIR SE PEUEDEN CAMBIAR LOS TIPOS DE VARIABLES, SI PRIMERO ERA UNA CADENA SE PUEDE CAMBAIR A ENTERO U OTRO.
+
+name = "miguel"
+print (type(name))
+
+name = 45
+print (type(name))
 
 
 #### LISTAS EN PYTHON 
