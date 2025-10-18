a6c751acfe779b5f921782da9a9d9c8a8c689845
M1guel0 <miguelangel-0419@hotmail.com>
2025-10-18 18:44:53 -0500

    ejercicios de listas

--- tutorialMidudev.py
+++ tutorialMidudev.py
@@ -67,4 +67,65 @@ print (lista [::-1]) # muestra los valores de la lista en orden inverso.
 
 #imprimir lista de listas. de elementos señalados.
 print (matrices [1] [0]) 
+#
+#Forma de concatenar una lista 
+#Esat es la forma menos eficeinte.
+lista = lista + [10, 11, 12, 13, 14]
+#print (lista)
+
+#Esta es la forma mas eficiente. 
+lista += [10, 11, 12, 13, 14]
+print (lista)
+
+# Ejercicio 2: Intercambio de posiciones
+# Dada la siguiente lista:
+numeros = [10, 20, 30, 40, 50]
+# Intercambia la primera y la última posición utilizando solo asignación por índice
+#### SOLUCIONES.
+print (numeros [::-1])
+numeros [0] = 50
+numeros [4] = 10
+print (numeros)
+
+# Ejercicio 3: El sándwich de listas
+# Dadas las siguientes listas:
+pan = ["pan arriba"]
+ingredientes = ["jamón", "queso", "tomate"]
+pan_abajo = ["pan abajo"]
+# Crea una lista llamada sandwich que contenga el pan de arriba, los ingredientes y el pan de abajo, en ese orden.
+
+Sandwich =[]
+#Sandwich.extend (pan + ingredientes + pan_abajo)
+Sandwich += (pan + ingredientes + pan_abajo)
+print(Sandwich)
+
+# Ejercicio 4: Duplicando la lista
+# Dada una lista:
+lista = [1, 2, 3]
+# Crea una nueva lista que contenga los elementos de la lista original duplicados.
+# Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
+
+lista += [1, 2, 3]
+print (lista)
+
+# Ejercicio 5: Extrayendo el centro
+# Dada una lista con un número impar de elementos, extrae el elemento que se encuentra en el centro de la lista utilizando slicing.
+# Ejemplo: lista = [10, 20, 30, 40, 50] -> El centro es 30
+lista = [10, 20, 30, 40, 50]
+print (lista [2:3])
+
+# Ejercicio 6: Reversa parcial
+# Dada una lista, invierte solo la primera mitad de la lista (utilizando slicing y concatenación).
+# Ejemplo: lista = [1, 2, 3, 4, 5, 6] -> Resultado: [3, 2, 1, 4, 5, 6]
+lista = [1, 2, 3, 4, 5, 6]
+#lista[0] = 3
+#lista[1] = 2
+#lista[2] = 1
+#print (lista)
+print (lista [-4:-7:-1])
+lista[:3] = [3, 2, 1]
+print (lista)
+
+len(lista) 
+
 
